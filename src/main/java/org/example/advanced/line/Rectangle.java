e9b273e29ece6e89bc885102af286a7280a86ece
Patka879 <mysiak.patrycja@gmail.com>
2023-04-26 10:03:51 +0200

    Line class refactored

--- src/main/java/org/example/advanced/line/Rectangle.java
+++ src/main/java/org/example/advanced/line/Rectangle.java
@@ -22,4 +22,24 @@ public class Rectangle {
     public char getFilling() {
         return filling;
     }
+
+    public void printRectangle() {
+        for (int i = 0; i < height; i++) {
+            if (i == 0 || i == height - 1) {
+                for (int j = 0; j < width; j++) {
+                    System.out.print(filling);
+                }
+                System.out.println("");
+            } else {
+                for (int j = 0; j < width; j++) {
+                    if (j == 0 || j == width - 1) {
+                        System.out.print(filling);
+                    } else {
+                        System.out.print(" ");
+                    }
+                    System.out.println("");
+                }
+            }
+        }
+    }
 }
--- src/main/java/org/example/advanced/line/SymbolsDemo.java
+++ src/main/java/org/example/advanced/line/SymbolsDemo.java
@@ -7,9 +7,17 @@ public class SymbolsDemo {
         Line line2 = new Line(5, '@');
         Line line3 = new Line(10, '&');
 
-        line.printLine();
-        line2.printLine();
-        line3.printLine();
+        Rectangle rectangle1 = new Rectangle(10, 20, '%');
+        Rectangle rectangle2 = new Rectangle(3,6, '$');
+        Rectangle rectangle3 = new Rectangle(5,6, '#');
+
+//        line.printLine();
+//        line2.printLine();
+//        line3.printLine();
+
+        rectangle1.printRectangle();
+        rectangle2.printRectangle();
+        rectangle3.printRectangle();
     }
 
 }
