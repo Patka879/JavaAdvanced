28b2c7e61a14806ab9c0431625ab19680f6b520a
Patka879 <mysiak.patrycja@gmail.com>
2023-04-26 14:25:56 +0200

    House created

--- src/main/java/org/example/advanced/house/HouseDemo.java
+++ src/main/java/org/example/advanced/house/HouseDemo.java
@@ -11,9 +11,11 @@ public class HouseDemo {
         Bed bed1 = new Bed(10);
         Bed bed2 = new Bed(2);
 
-        Room room1 = new Room();
-
         Window[] windows = {window1, window2, window3, window4};
+
+        Room room1 = new Room(bed1, windows);
+
+
 
         for (int i = 0; i < windows.length; i++) {
             System.out.println("Window no. " + (i + 1) + " " + windows[i].isWindowOpen());
--- src/main/java/org/example/advanced/house/Room.java
+++ src/main/java/org/example/advanced/house/Room.java
@@ -4,11 +4,8 @@ public class Room extends House {
     private Window[] windows;
     private Bed bed;
 
-    public Room() {
-        this.bed = new Bed(5);
-        this.windows = new Window[2];
-        for (int i = 0; i < 2; i++) {
-            windows[i] = new Window(true);
-        }
+    public Room(Bed bed, Window[] windows) {
+        this.bed = bed;
+        this.windows = windows;
     }
 }
