d50d9d9e43764dcb9f153f00f3ab86e989e83fe0
Patka879 <mysiak.patrycja@gmail.com>
2023-05-02 15:11:27 +0200

    Bookshelf created

---
+++ src/main/java/org/example/advanced/cards/PlayersDecksGenerator.java
@@ -0,0 +1,13 @@
+package org.example.advanced.cards;
+
+public class PlayersDecksGenerator {
+}
+
+
+// main method is only for creating objects and starting
+// logic should be in classes
+
+// horizontalPosition = newHorizontalPosition and then use getters to acces them
+
+// print the board in Game
+
