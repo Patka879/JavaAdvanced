eb1104751ffeaec3635b2be01ca4f7419e7f7138
Patka879 <mysiak.patrycja@gmail.com>
2023-05-05 10:56:36 +0200

    Functional Interfaces Examples

--- src/main/java/org/example/advanced/streams/functionalInterfaces/task35/generator.java
+++ src/main/java/org/example/advanced/streams/functionalInterfaces/task35/generator.java
@@ -2,8 +2,6 @@ package org.example.advanced.streams.functionalInterfaces.task35;
 
 import java.util.Comparator;
 import java.util.Random;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -28,15 +26,21 @@ public class generator {
         // Comparing two strings along the length (difficult).
         Comparator<String> compareLength = (str1, str2)-> {
             int result;
-            if (str1.length() >= str2.length()) {
+            if (str1.length() > str2.length()) {
                 result = str1.length();
-            } else  {
+                System.out.println("Longer string: " + str1);
+            } else if (str1.length() < str2.length())  {
                 result = str2.length();
+                System.out.println("Longer string: " + str2);
+            } else {
+                result = str1.length();
+                System.out.println("Both strings are equal");
             }
             return result;
         };
 
         System.out.println(compareLength.compare("Lambda", "Expression"));
+        System.out.println(compareLength.compare("Dog", "Cat"));
     }
 
 }
