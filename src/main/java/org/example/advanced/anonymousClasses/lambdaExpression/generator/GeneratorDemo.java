2f00dbfd2f3b9ab8f72796ef400c4f6e3b7f433f
Patka879 <mysiak.patrycja@gmail.com>
2023-05-05 09:20:28 +0200

    Generator Corrected

--- src/main/java/org/example/advanced/anonymousClasses/lambdaExpression/generator/GeneratorDemo.java
+++ src/main/java/org/example/advanced/anonymousClasses/lambdaExpression/generator/GeneratorDemo.java
@@ -6,9 +6,11 @@ public class GeneratorDemo {
     public static void main(String[] args) {
         Generator randomNumber = () -> {
             Random random = new Random();
-            return random.nextInt(0,10);
+            return random.nextInt(0,11);
         };
 
+        System.out.println(randomNumber.generator());
+        System.out.println(randomNumber.generator());
         System.out.println(randomNumber.generator());
     }
 }
